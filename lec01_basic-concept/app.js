d42a882132f6cc865afca5ee979dbfde28f457ca
Jooka <mr.jooka@gmail.com>
2022-08-07 03:37:28 +0900

    [1.Basic Concept] app.js 실행

--- lec01_basic-concept/app.js
+++ lec01_basic-concept/app.js
@@ -6,5 +6,10 @@ function rqListener(req, res) {
 
 const server = http.createServer(rqListener);
 
-// 서버가 구동되게 만들려면 listen 메서드를 사용해야한다
-server.listen();
+server.listen(
+  // 첫번째 인수는 port 번호이다
+  3000
+);
+
+/* 이제 Terminal에서 node app.js 타이핑해서 파일을 실행하면 터미널의 명령입력창은 비활성화된다.
+왜냐하면 app.js의 실행이 끝나지 않았기 때문이다 */
