bde34b7c7948d571a14c86780dd6b53d386f4f4a
Jooka <mr.jooka@gmail.com>
2022-08-07 03:31:16 +0900

    [1.Basic Concept] createServer가 실행되지 않는 이유

--- lec01_basic-concept/app.js
+++ lec01_basic-concept/app.js
@@ -4,5 +4,6 @@ function rqListener(req, res) {
   console.log(req);
 }
 
-// 서버에 요청이 들어올 때마다 createSever의 매개변수인 callback 함수가 실행된다
+// app.js 를 실행해도 console에 아무것도 안찍히는 이유는 아래 서버가 한번 실행만 되고 메모리에 저장되지 않았기 때문이다.
+// const server에 선언해줘야한다(할당해야한다).
 http.createServer(rqListener);
