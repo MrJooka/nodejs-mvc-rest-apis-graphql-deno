247bf318b7e436f1ba59e83bcdced6391be025c8
Jooka <mr.jooka@gmail.com>
2022-08-07 03:32:44 +0900

    [1.Basic Concept] server 선언 및 listen 메서드 실행

--- lec01_basic-concept/app.js
+++ lec01_basic-concept/app.js
@@ -4,6 +4,7 @@ function rqListener(req, res) {
   console.log(req);
 }
 
-// app.js 를 실행해도 console에 아무것도 안찍히는 이유는 아래 서버가 한번 실행만 되고 메모리에 저장되지 않았기 때문이다.
-// const server에 선언해줘야한다(할당해야한다).
-http.createServer(rqListener);
+const server = http.createServer(rqListener);
+
+// 서버가 구동되게 만들려면 listen 메서드를 사용해야한다
+server.listen();
