8ab344b44537c52423aacb8c355284df6e4b742f
Jooka <mr.jooka@gmail.com>
2022-08-07 03:44:41 +0900

    [1.Basic Concept] access localhost:3000 at Broswer

--- lec01_basic-concept/app.js
+++ lec01_basic-concept/app.js
@@ -13,3 +13,5 @@ server.listen(
 
 /* 이제 Terminal에서 node app.js 타이핑해서 파일을 실행하면 터미널의 명령입력창은 비활성화된다.
 왜냐하면 app.js의 실행이 끝나지 않았기 때문이다 */
+
+// 브라우저에서 localhost:3000을 입력하면 terminal console.log(req)가 실행되어 req를 표시해준다
