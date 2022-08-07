188a56d27c655635f1b99601bd64e983b816713b
MrJooka <mr.jooka@gmail.com>
2022-08-08 01:51:34 +0900

    오류 해결 방법

--- lec01_basic-concept/app.js
+++ lec01_basic-concept/app.js
@@ -21,13 +21,15 @@ const server = http.createServer((req, res) => {
       body.push(chunk);
     });
 
-    req.on("end", () => {
+    return req.on("end", () => {
       const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split("=")[1];
       fs.writeFileSync("message.txt", message);
       /*  하지만 아래코드가 리스너에서 등록되어 버린다면 16번째줄의 if문에서의 return이 사라져 38번째 코드 가 실행되어버린다.
           리스너는 비동기로 실행되기 때문에 43번째에서 응답을 종료해버린 후
           다시 비동기인 req.on('end) 리스너가 실행되기 때문에 res.end()를 다시 실행시키면서 에러를 발생시킨다
+
+          ==> req.on('end')구문 자체를 return 시켜서 if문 아래 코드가 실행되지 않게 한다
       */
       res.statusCode = 302;
       res.setHeader("Location", "/");
