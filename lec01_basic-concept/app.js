ae43c758a91efec53d7525bd9e4bec9db4bb9dfc
MrJooka <mr.jooka@gmail.com>
2022-08-07 18:41:12 +0900

    [1.Basic Concept] res.setHeader 및 res.write로 응답하기

--- lec01_basic-concept/app.js
+++ lec01_basic-concept/app.js
@@ -1,15 +1,16 @@
 const http = require("http");
 
-function rqListener(req, res) {
-  console.log(req);
-  /*  이벤트 루프를 종료시키는 메서드이다 */
-  process.exit();
-}
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method, req.headers);
 
-const server = http.createServer(rqListener);
+  /* setHeader를 설정하지 않으면 브라우저에서 response가 어떤 데이터 타입인지 알 수 없기 때문에 읽지 못한다 */
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Sever</title></head>");
+  res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
+  res.write("</html>");
+  /* end() 메서드 실행한 이후에는 res 부분의 내용을 수정할 수 없다 */
+  res.end();
+});
 
-/* listen메서드를 실행함으로써 event loop가 계속 실행된다. */
-server.listen(
-  // 첫번째 인수는 port 번호이다
-  3000
-);
+server.listen(3000);
