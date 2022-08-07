08e43a5c17df66514a2a964e4d7b6315635cc4b7
MrJooka <mr.jooka@gmail.com>
2022-08-07 18:54:02 +0900

    1. 라우터 요청

--- lec01_basic-concept/app.js
+++ lec01_basic-concept/app.js
@@ -1,9 +1,20 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
+  const url = req.url;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    /* form 태그의 action에 "/messgae"를 입력했기 때문에 무언가를 입력하고 Send버튼을 누르면
+       localhost:3000/message 페이지로 이동한다  */
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text"><button type="submit">Send</button></form></body>'
+    );
+    res.write("</html>");
+    /* res.end()를 return 안해도 실행은 되지만 return으로 익명함수 실행을 종료한다. 리턴문 아래 코드들은 실행 안되게 하기 위해서다 */
+    return res.end();
+  }
 
-  /* setHeader를 설정하지 않으면 브라우저에서 response가 어떤 데이터 타입인지 알 수 없기 때문에 읽지 못한다 */
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<head><title>My First Sever</title></head>");
