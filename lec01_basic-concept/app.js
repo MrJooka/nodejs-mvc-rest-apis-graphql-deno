2199ada41474515fdf8e4e8b2663c447419cc095
MrJooka <mr.jooka@gmail.com>
2022-08-08 01:38:39 +0900

    이해를 잘못해서 수정함

--- lec01_basic-concept/app.js
+++ lec01_basic-concept/app.js
@@ -22,14 +22,14 @@ const server = http.createServer((req, res) => {
     });
 
     /* 실행순서 */
-    // 2. 'end'이벤트가 끝날 때 실행됨. 즉, res.end()가 실행된 후에 아래 이벤트 리스너 콜백함수가 실행됨
+    // 2. 수정함:: 'end'이벤트가 끝날 때 실행됨. 모든 chunk를 다 받은 후에 아래 이벤트 리스너 콜백함수가 실행됨
     req.on("end", () => {
       const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split("=")[1];
       fs.writeFileSync("message.txt", message);
     });
 
-    //1. req.on은 이벤트 리스너를 등록만 한것이고 아래 코드는 바로 평가됨
+    //1. req.on은 이벤트 리스너를 등록만 한것이고 아래 코드는 바로 평가됨. 그러므로 req.on("end", callback)의 두번째 인수인 콜백 함수 구문에서 실행되어야함
     res.statusCode = 302;
     res.setHeader("Location", "/");
     return res.end();
