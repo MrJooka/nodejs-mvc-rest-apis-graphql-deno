2aa29b0871d1bc13dad2911db211ee00483ff2d1
MrJooka <mr.jooka@gmail.com>
2022-08-08 01:35:24 +0900

    1. 이벤트 기반코드 실행의 이해 1-1

--- lec01_basic-concept/app.js
+++ lec01_basic-concept/app.js
@@ -15,15 +15,21 @@ const server = http.createServer((req, res) => {
   }
   if (url === "/message" && method === "POST") {
     const body = [];
+
     req.on("data", (chunk) => {
       console.log(chunk);
       body.push(chunk);
     });
+
+    /* 실행순서 */
+    // 2. 'end'이벤트가 끝날 때 실행됨. 즉, res.end()가 실행된 후에 아래 이벤트 리스너 콜백함수가 실행됨
     req.on("end", () => {
       const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split("=")[1];
       fs.writeFileSync("message.txt", message);
     });
+
+    //1. req.on은 이벤트 리스너를 등록만 한것이고 아래 코드는 바로 평가됨
     res.statusCode = 302;
     res.setHeader("Location", "/");
     return res.end();
