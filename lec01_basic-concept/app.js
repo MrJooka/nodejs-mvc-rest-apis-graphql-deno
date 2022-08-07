2908cee975fc1810d5fd68dbc31fc1ae89428269
MrJooka <mr.jooka@gmail.com>
2022-08-08 01:50:08 +0900

    이벤트 리스너에 res 설정시 오류 발생

--- lec01_basic-concept/app.js
+++ lec01_basic-concept/app.js
@@ -21,18 +21,18 @@ const server = http.createServer((req, res) => {
       body.push(chunk);
     });
 
-    /* 실행순서 */
-    // 2. 수정함:: 'end'이벤트가 끝날 때 실행됨. 모든 chunk를 다 받은 후에 아래 이벤트 리스너 콜백함수가 실행됨
     req.on("end", () => {
       const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split("=")[1];
       fs.writeFileSync("message.txt", message);
+      /*  하지만 아래코드가 리스너에서 등록되어 버린다면 16번째줄의 if문에서의 return이 사라져 38번째 코드 가 실행되어버린다.
+          리스너는 비동기로 실행되기 때문에 43번째에서 응답을 종료해버린 후
+          다시 비동기인 req.on('end) 리스너가 실행되기 때문에 res.end()를 다시 실행시키면서 에러를 발생시킨다
+      */
+      res.statusCode = 302;
+      res.setHeader("Location", "/");
+      return res.end();
     });
-
-    //1. req.on은 이벤트 리스너를 등록만 한것이고 아래 코드는 바로 평가됨. 그러므로 req.on("end", callback)의 두번째 인수인 콜백 함수 구문에서 실행되어야함
-    res.statusCode = 302;
-    res.setHeader("Location", "/");
-    return res.end();
   }
 
   res.setHeader("Content-Type", "text/html");
